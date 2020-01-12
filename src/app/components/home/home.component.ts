a2c9d7f4d1940857eee58489ffd68bdef6a8c2ea
Zsolt Schutzbach <succ92@gmail.com>
2020-01-12 10:23:47 +0100

    Changes based on feedback

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Component, OnInit, OnDestroy, HostBinding } from '@angular/core';
 import * as moment from 'moment';
 import { BehaviorSubject, timer, Subject } from 'rxjs';
 import { takeUntil } from 'rxjs/operators';
